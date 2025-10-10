c0370b6988233910acbb8d02541620d899892480
Oleksandr Petryshyn <alexlikenew@gmail.com>
2025-10-10 12:12:15 +0200

    Updated vercel speed index complexity

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import AppLayout from "./ui/AppLayout.jsx";
 import Index from "./pages/Index.jsx";
 // import Tailwind (musi być pierwszy!)
-import SpeedInsights from "@vercel/speed-insights/astro"
+
 import "./styles/index.css";
 
 // import SCSS (custom)
@@ -15,26 +15,29 @@ import Contact from "./pages/Contact.jsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import BlogPage from "./pages/BlogPage.jsx";
 import FAQ from "./pages/FAQ.jsx";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 function App() {
     const [count, setCount] = useState(0)
     const queryClient = new QueryClient();
 
     return (
-        <QueryClientProvider client={queryClient}>
+        <>
             <SpeedInsights/>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<AppLayout/>}>
-                        <Route index path="/" element={<Index/>}/>
-                        <Route path="/faq" element={<FAQ/>}/>
-                        <Route path="/blog" element={<Blog/>}/>
-                        <Route path="/blog/:url" element={<BlogPage/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-            // </QueryClientProvider>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<AppLayout/>}>
+                            <Route index path="/" element={<Index/>}/>
+                            <Route path="/faq" element={<FAQ/>}/>
+                            <Route path="/blog" element={<Blog/>}/>
+                            <Route path="/blog/:url" element={<BlogPage/>}/>
+                            <Route path="/contact" element={<Contact/>}/>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+                // </QueryClientProvider>
+        </>
     )
 }
 
